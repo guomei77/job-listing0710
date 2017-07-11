2d82ec3e1060dad37786ba110ce6d167f22866a7
guomei <lengxuehx@gmail.com>
2017-07-11 15:53:09 +0800

    implement part4

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -7,7 +7,7 @@ class Admin::JobsController < ApplicationController
     end
 
     def index
-      @jobs = Job.all
+      @jobs = Job.where(:is_hidden => false)
     end
 
     def new
@@ -49,7 +49,7 @@ class Admin::JobsController < ApplicationController
     private
 
     def job_params
-      params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :conatct_email)
+      params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :conatct_email, :is_hidden)
     end
 
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,6 @@
 class Job < ApplicationRecord
   validates :title, presence: true
-  validates :wage_upper_bound, presence: true
+  validates :wage_upper_bound, presence: true, numericality: {greater_than: :wage_lower_bound,message: "薪水下限不能高于薪水上限"}
   validates :wage_lower_bound, presence:true, numericality: {greater_than: 0}
 
 
